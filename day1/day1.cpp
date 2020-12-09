6a7accf1a9b018a92fbbf761abf5844b5e74b882
Steve McCoy <stephen.a.mccoy@gmail.com>
2020-12-09 23:44:25 +0000

    Completed day 2

---
+++ day1/day1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void main() {
+  int d1, d2;
+
+  std::cout << "Day 1. Two entries that sum to 2020 are:" << d1 << " + " << d2 << std::endl;
+}
--- day2/day2.pl
+++ day2/day2.pl
@@ -1,9 +1,69 @@
-% -*- prolog -*-
-% Advent of Code 2020 - Day 2
+%
+% Advent of Code 2020 - Day 2.
+%
 
-validate_all :-
-    
+:- working_directory(_, 'C:/Users/stephen.mccoy/github/aoc2020/day2/').
+:- dynamic input/1.
 
-    
-day2part1 :-
-    validate_all.
+read_input_lines(List) :-
+	findall(X, input(X), List).
+
+read_input(List) :-
+	retractall(input(_)),
+	load_files(['input.pl'], []),
+	!,
+	read_input_lines(List).
+
+% Count number of occurrences in a list.
+count_matches(X, [X | Tail], N) :-
+	!,
+	count_matches(X, Tail, M),
+	N is M + 1.
+count_matches(X, [_ | Tail], N) :-
+	!,
+	count_matches(X, Tail, N).
+count_matches(_, [], 0).
+
+% Get indexed item in a list.
+get_item(1, [Item | _], Item).
+get_item(N, [_ | Tail], Item) :-
+	N > 1,
+	M is N -1,
+	get_item(M, Tail, Item).
+
+
+% Verify the given example against the rule.
+verify_part1([Min,Max,Item], Example) :-
+	atom_chars(Example, Atoms),
+	count_matches(Item, Atoms, Count),
+	Count =< Max,
+	Count >= Min.
+
+verify_part2([Pos1, Pos2, Item], Example) :-
+	atom_chars(Example, Atoms),
+	findall(X, 
+		(	member(X,[Pos1,Pos2]), 
+			get_item(X, Atoms, Item)
+		), L),
+	length(L, 1).
+
+day2part1(Good) :-
+	read_input(InputList),
+	findall(Example, 
+		(	member([Min,Max,Item,Example], InputList), 
+			verify_part1([Min,Max,Item],Example)), 
+		Good),
+	length(Good, GoodCount),
+	format('In Part 1, ~d examples were good.', [GoodCount]).
+
+day2part2(Good) :-
+	read_input(InputList),
+	findall(Example, 
+		(	member([Min,Max,Item,Example], InputList), 
+			verify_part2([Min,Max,Item],Example)
+		), 
+		Good),
+	length(Good, GoodCount),
+	format('In Part 2, ~d examples were good.', [GoodCount]).
+
+
